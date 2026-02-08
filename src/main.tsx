dd85ca23eeafa591e4b572edbc03c6889fdb36d9
karelina2507-spec <karelina2507@gmail.com>
2026-02-08 14:32:18 -0300

    Updated index.html

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,15 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registrado:', registration);
+      })
+      .catch((error) => {
+        console.log('Error al registrar Service Worker:', error);
+      });
+  });
+}
